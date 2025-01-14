b965c6c4267f75bc9e78722cee3140660ba6b960
fereline0 <shimko.alex.fereline@gmail.com>
2025-01-14 12:19:42 +0300

    release

--- resources/views/users/partials/update-resume-form.blade.php
+++ resources/views/users/partials/update-resume-form.blade.php
@@ -1,10 +1,10 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Резюме') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Здесь вы можете обновить свое резюме, чтобы работодатели могли лучше понять ваши ожидания, навыки и опыт работы.') }}
         </p>
     </header>
